d232dbbf095e5e1ab63bba829782dc64622f60f2
admin <admin@admin.local>
2024-10-02 15:22:46 +0200

    README.MD edited for Firefox

--- build.sh
+++ build.sh
@@ -3,4 +3,3 @@ mkdir tmp
 cp content.js tmp/content.js
 cp manifest.json tmp/manifest.json
 cp -r images tmp/images
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --pack-extension=tmp
